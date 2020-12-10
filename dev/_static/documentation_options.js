abec59fda8da4af04723067cfdac38ba921840b5
TravisCI <travis@nothing.com>
2020-12-10 17:57:43 +0000

    Deploy dev from TravisCI

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.50.g2aa4aab',
+    VERSION: 'v0.2.2.dev16+g6146a0f',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
